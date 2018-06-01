daf37c2c66b4236d9763661807a109e370eb6a76
Samy Pessé <samypesse@gmail.com>
2018-06-01 07:40:55 +0200

    Update exercise for course: redis for pubsub

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Whiteboard
 
 Base de code pour les exercices de cours.
+
+### Exercise 3:
+
+Utiliser `redis` pour broadcaster les messages à travers plusieurs instances de l'application.
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
 const uuidv4 = require("uuid/v4");
 
 const app = express();
@@ -7,8 +9,84 @@ const app = express();
 const PUBLIC_FOLDER = path.join(__dirname, "../public");
 const PORT = process.env.PORT || 5000;
 
-// Completer ce fichier
+const socketsPerChannels /* Map<string, Set<WebSocket>> */ = new Map();
+const channelsPerSocket /* WeakMap<WebSocket, Set<string> */ = new WeakMap();
 
+// Initialize a simple http server
+const server = http.createServer(app);
+
+// Initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+/*
+ * Subscribe a socket to a specific channel.
+ */
+function subscribe(socket, channel) {
+    let socketSubscribed = socketsPerChannels.get(channel) || new Set();
+    let channelSubscribed = channelsPerSocket.get(socket) || new Set();
+
+    socketSubscribed = socketSubscribed.add(socket);
+    channelSubscribed = channelSubscribed.add(channel);
+
+    socketsPerChannels.set(channel, socketSubscribed);
+    channelsPerSocket.set(socket, channelSubscribed);
+}
+
+/*
+ * Unsubscribe a socket from a specific channel.
+ */
+function unsubscribe(socket, channel) {
+    let socketSubscribed = socketsPerChannels.get(channel) || new Set();
+    let channelSubscribed = channelsPerSocket.get(socket) || new Set();
+
+    socketSubscribed.delete(socket);
+    channelSubscribed.delete(channel);
+
+    socketsPerChannels.set(channel, socketSubscribed);
+    channelsPerSocket.set(socket, channelSubscribed);
+}
+
+/*
+ * Subscribe a socket from all channels.
+ */
+function unsubscribeAll(socket) {
+    const channelSubscribed = channelsPerSocket.get(socket) || new Set();
+
+    channelSubscribed.forEach(channel => {
+        unsubscribe(socket, channel);
+    });
+}
+
+/*
+ * Broadcast a message to all sockets connected to this server.
+ */
+function broadcastToSockets(channel, data) {
+    const socketSubscribed = socketsPerChannels.get(channel) || new Set();
+
+    socketSubscribed.forEach(client => {
+        client.send(data);
+    });
+}
+
+// Broadcast message from client
+wss.on("connection", ws => {
+    ws.on('close', () => {
+        unsubscribeAll(ws);
+    });
+
+    ws.on("message", data => {
+        const message = JSON.parse(data.toString());
+
+        switch (message.type) {
+            case 'subscribe':
+                subscribe(ws, message.channel);
+                break;
+            default:
+                broadcastToSockets(message.channel, data);
+                break;
+        }
+    });
+});
 
 // Assign a random channel to people opening the application
 app.get("/", (req, res) => {
@@ -24,3 +102,7 @@ app.get("/:channel", (req, res, next) => {
 });
 
 app.use(express.static(PUBLIC_FOLDER));
+
+server.listen(PORT, () => {
+    console.log(`Server started on port ${server.address().port}`);
+});
